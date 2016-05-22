dc901f1de27ab709e1e9405bbec0cd265b03db0e
Youwei <zhanyouwei@icloud.com>
2016-05-22 15:32:56 +0800

    完成添加职位；完善路由；完成职位管理数据获取；

--- routes-admin/index.js
+++ routes-admin/index.js
@@ -5,17 +5,147 @@
 
 var express = require('express');
 var router = express.Router();
+var Job = require('../models/job');
+var JobTitle = require('../models/jobTitle');
+var Degree = require('../models/degree');
+var async = require('async');
 
-router.get('/waniuadmin', function (req, res) {
-  res.render("admin/index");
+router.get('/waniuadmin', function (req, res, next) {
+  Job.find({}, function (err, results) {
+    if (err) {
+      next(err);
+      return;
+    }
+    //TODO: 方维 - 需要将jobTitle 的 值转换为 中文值
+    res.render("admin/index", {jobs: results});
+  });
 });
 
-router.get('/newjob', function (req, res) {
-  res.render("admin/newjob");
+router.get('/newjob', function (req, res, next) {
+  JobTitle.find({}, function (err, results) {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.render("admin/newjob", {jobTitle: results});
+  });
+});
+
+router.get('/editjob/:id', function (req, res, next) {
+  async.parallel([
+    function (callback) {
+      Job.find({_id: req.params.id}, function (err, results) {
+        if (err) {
+          callback(err);
+          return;
+        }
+        callback(null, results);
+      });
+    },
+    function (callback) {
+      JobTitle.find({}, function (err, results) {
+        if (err) {
+          callback(err);
+          return;
+        }
+        callback(null, results);
+      });
+    }
+  ], function (err, results) {
+    if (err) {
+      next(err);
+      return;
+    }
+    if (results[0].length) {
+      console.log({job: results[0], jobTitle: results[1]});
+      res.render("admin/editjob", {job: results[0], jobTitle: results[1]});
+    } else {
+      res.render('404');
+    }
+  });
+
 });
 
 router.get('/candidate', function (req, res) {
   res.render("admin/candidate");
+});
+
+
+//职位置顶
+router.put('/job/top', function (req, res) {
+
+  var id = req.body.id;
+  if (id === undefined) {
+    res.json({state: 1, msg: 'id不存在'});
+    return false;
+  } else {
+
+    Job.findById(id, function (err, job) {
+      if (job === undefined) {
+        res.json({state: 1, msg: '职位不存在'});
+        return false;
+      }
+      //找到weight值最大的那个
+      Job.findOne({})
+        .sort({weight: -1})
+        .exec(function (err, wJob) {
+
+          job.weight = wJob.weight + 1;
+          job.save(function (err, rtJob) {
+
+            if (err) {
+              console.log(err);
+              res.json({state: 1, msg: '保存数据错误'});
+            }
+            else {
+              res.json({state: 0, job: rtJob});
+            }
+
+          })
+
+        })
+
+    })
+
+  }
+})
+
+
+//职位新增
+router.post('/job', function (req, res) {
+
+
+  var reqJob = req.body.job;
+
+
+  if (reqJob === undefined || reqJob.jobTitle === undefined || reqJob.minSalary === undefined
+    || reqJob.city === undefined) {
+
+    res.sendStatus(500);
+    res.send({code: 500, message: '输入均不得为空'});
+
+    return false;
+  }
+
+  var job = new Job({
+
+    jobTitle: reqJob.jobTitle,          //职位id
+    minSalary: reqJob.minSalary,         //最小薪水
+    maxSalary: reqJob.maxSalary,         //最大薪水
+    city: reqJob.city,                 //期望城市
+    degree: reqJob.degree,                 //学历要求
+    attraction: reqJob.attraction,           //职位诱惑
+    description: reqJob.description
+
+  });
+
+  job.save(function (error, pJob) {
+    if (error) {
+      res.sendStatus(500);
+      res.send({code: 500, message: '保存job异常'});
+    }
+    res.send(pJob);
+  });
 });
 
 //职位修改
@@ -42,7 +172,7 @@ router.put('/job', function (req, res) {
       res.json({
         state: 1,
         msg: '该job不存在'
-      })
+      });
 
       return false;
     }
@@ -67,48 +197,5 @@ router.put('/job', function (req, res) {
   })
 });
 
-//职位新增
-router.post('/job', function (req, res) {
-
-  var reqJob = req.body.job;
-
-  if(reqJob===undefined || reqJob.jobTitle=== undefined || reqJob.minSalary === undefined
-    || reqJob.city === undefined ){
-
-    res.json({
-
-      state: 1,
-      msg: '输入均不得为空'
-
-    })
-
-    return false;
-  }
-
-
-  var job = new Job({
-
-    jobTitle: reqJob.jobTitle,          //职位id
-    minSalary: reqJob.minSalary,         //最小薪水
-    maxSalary: reqJob.maxSalary,         //最大薪水
-    city: reqJob.city,                 //期望城市
-    degree: reqJob.degree,                 //学历要求
-    attraction: reqJob.attraction,           //职位诱惑
-    description: reqJob.description,
-
-  });
-
-  job.save(function (error, pJob) {
-
-    if (error) console.log(error);
-
-    res.json({
-      state: 0,
-      job: pJob
-    })
-  });
-
-
-});
 
 module.exports = router;
--- test/index.test.js
+++ test/index.test.js
@@ -19,13 +19,13 @@ describe('routes/index.js', function () {
 
   //修改job
   var reqModifyJob = {
-    id: "573c045dc0e2fd0d280e51e9",         //id
+    id: "574042341a9659ab0ec0611e",         //id
     jobTitle: "573bfec51699b1da6e16186b",    //jobtitles的id
     minSalary: 1000,         //最小薪水
     maxSalary: 3000,         //最大薪水
     city: '北京',           			 //期望城市
     degree: '研究生',	        	     //学历要求
-    attraction: ['妹子多', '福利好','升值快'],         	 //职位诱惑
+    attraction: ['妹子多', '福利好', '升值快'],         	 //职位诱惑
     description: '你，就是下一个俞敏洪',
 
   };
@@ -38,7 +38,7 @@ describe('routes/index.js', function () {
     maxSalary: 3000,         //最大薪水
     city: '北京',                  //期望城市
     degree: '研究生',                 //学历要求
-    attraction: ['妹子多', '福利好','升值快'],            //职位诱惑
+    attraction: ['妹子多', '福利好', '升值快'],            //职位诱惑
     description: '你，就是下一个俞敏洪',
 
   };
@@ -46,20 +46,35 @@ describe('routes/index.js', function () {
 
   describe('职位管理', function () {
 
-    // it('创建job', function (done) {
-    //   request.post('/job')
-    //     .send({
-    //       job: reqJob
-    //     })
-    //     .expect(200, function (err, res) {
-    //       should.not.exist(err);
-    //       res.body.state.should.containEql(0);
-    //       done();
-    //     })
-    // })
+
+    it('置顶job', function (done) {
+      request.put('/admin/job/top')
+        .send({
+          id: '574042341a9659ab0ec0611e'
+        })
+        .expect(200, function (err, res) {
+          should.not.exist(err);
+          res.body.state.should.containEql(1);
+
+          done();
+        })
+    })
+
+    it('创建job', function (done) {
+      request.post('/admin/job')
+        .send({
+          job: reqJob
+        })
+        .expect(200, function (err, res) {
+          should.not.exist(err);
+          res.body.state.should.containEql(0);
+
+          done();
+        })
+    })
 
     it('修改job成功', function (done) {
-      request.put('/job')
+      request.put('/admin/job')
         .send({
           job: reqModifyJob
         })
@@ -71,7 +86,7 @@ describe('routes/index.js', function () {
     })
 
     it('修改job失败', function (done) {
-      request.put('/job')
+      request.put('/admin/job')
         .send({
           job: errorJob
         })
@@ -82,37 +97,45 @@ describe('routes/index.js', function () {
         })
     })
 
+
+    it('职位详细信息', function (done) {
+      request.get('/job/57403cd1504d24c10cb52c9b')
+
+
+        .expect(200, function (err, res) {
+          should.not.exist(err);
+          res.body.state.should.containEql(0);
+          done();
+        })
+    })
+
+  }) //职位管理
+
+  describe('findJobTitles', function () {
+
+    it('查询所有职位名称', function (done) {
+      request.get('/jobTitle')
+        .expect(200, function (err, res) {
+          should.not.exist(err);
+          res.body.state.should.containEql(0);
+
+          done();
+        })
+    })
+
+
+  });
+
+  describe('findJobs', function () {
+
+    it('查询职位信息', function (done) {
+      request.get('/jobs/1')
+        .expect(200, function (err, res) {
+          should.not.exist(err);
+          res.body.state.should.containEql(0);
+
+          done();
+        })
+    })
   })
-
-  // describe('findJobTitles', function () {
-
-  //   it('查询所有职位名称', function (done) {
-  //     request.get('/jobTitle')
-  //       .expect(200, function (err, res) {
-  //         // should.not.exist(err);
-  //         // res.body.state.should.containEql(0);
-  //         console.log(res.body.jobTitles);
-  //         done();
-  //       })
-  //   })
-
-
-  // })
-
-  // describe('findJobs', function () {
-
-  //   it('查询职位信息', function (done) {
-  //     request.get('/job/1')        
-  //       .expect(200, function (err, res) {
-  //         // should.not.exist(err);
-  //         // res.body.state.should.containEql(0);
-  //         console.log(res.body.jobs);
-  //         done();
-  //       })
-  //   })
-
-
-  // })
-
-
-})
+});
