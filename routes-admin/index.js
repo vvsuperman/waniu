5b15b933f59ea7d8c559486688f17bdeec0418b8
Jason <zhanyouwei@meitunmama.com>
2016-06-03 18:22:09 +0800

    报表

--- routes-admin/index.js
+++ routes-admin/index.js
@@ -224,6 +224,10 @@ router.get('/applylist/:id', routerFilter.authorize, function (req, res, next) {
 
 });
 
+router.get('/reportforms', function (req, res) {
+  res.render('admin/report-forms');
+});
+
 //职位置顶
 router.post('/job/top', routerFilter.authorize, function (req, res) {
   var id = req.body.id;
